20fbd95d05ab2db17ecd9767602f8bad2d396245
Deakhadevi <psdurgadevi@gmail.com>
2023-01-04 21:18:53 +0530

    Durgadevi |First Ruby program

---
+++ 1.rb
@@ -0,0 +1,19 @@
+name="Durga"
+Age=20
+print "Your name is #{name}"
+require 'colorize'
+puts name.colorize(:blue)
+
+
+=begin
+print "Whats your name?"
+name=gets.chomp
+print "How old are you ? "
+age = gets.chomp
+print "How tall are you ?"
+height = gets.chomp
+puts " You name is #{name} and age is #{age} year old and your height is #{height} cms"
+#end
+
+
+
